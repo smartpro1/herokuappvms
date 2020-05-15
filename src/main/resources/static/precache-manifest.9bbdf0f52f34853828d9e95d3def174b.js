3428221078401a07930849541fb4c40d70a37cc1
smartpro1 <akeni.promise@yahoo.com>
2020-05-15 22:15:38 +0100

    react build add

---
+++ src/main/resources/static/precache-manifest.9bbdf0f52f34853828d9e95d3def174b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63526022439df2d36381dc1f755ae9dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5287710628bffae9df7a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1fcede7d1fe81ec07976",
+    "url": "/static/css/main.3c5e933f.chunk.css"
+  },
+  {
+    "revision": "5287710628bffae9df7a",
+    "url": "/static/js/2.013d38ce.chunk.js"
+  },
+  {
+    "revision": "8562cb9918f9e3abbf5e0a629e643285",
+    "url": "/static/js/2.013d38ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcede7d1fe81ec07976",
+    "url": "/static/js/main.7882133a.chunk.js"
+  },
+  {
+    "revision": "bbd485fd3ce0ff386d18",
+    "url": "/static/js/runtime-main.30f4288b.js"
+  },
+  {
+    "revision": "2a2e91ab0584eb9b6786b28adb449b0a",
+    "url": "/static/media/img2.2a2e91ab.png"
+  }
+]);
