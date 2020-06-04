93e94aaa581a346c5963bb1ebfdbf4049f2c4699
smartpro1 <akeni.promise@yahoo.com>
2020-06-04 23:35:19 +0100

    react landing bug fix

---
+++ src/main/resources/static/precache-manifest.0dcab90fec101efd21fa5657d8761de5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2488d7b6fe0e3cf57771d2837eafec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5287710628bffae9df7a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5d6f74387e71613fdb10",
+    "url": "/static/css/main.b7f2b02b.chunk.css"
+  },
+  {
+    "revision": "5287710628bffae9df7a",
+    "url": "/static/js/2.013d38ce.chunk.js"
+  },
+  {
+    "revision": "8562cb9918f9e3abbf5e0a629e643285",
+    "url": "/static/js/2.013d38ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6f74387e71613fdb10",
+    "url": "/static/js/main.aba86997.chunk.js"
+  },
+  {
+    "revision": "bbd485fd3ce0ff386d18",
+    "url": "/static/js/runtime-main.30f4288b.js"
+  },
+  {
+    "revision": "2a2e91ab0584eb9b6786b28adb449b0a",
+    "url": "/static/media/img2.2a2e91ab.png"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ed9876dbf00ced48241d2ced0ba6364.js"
+  "/precache-manifest.0dcab90fec101efd21fa5657d8761de5.js"
 );
 
 self.addEventListener('message', (event) => {
